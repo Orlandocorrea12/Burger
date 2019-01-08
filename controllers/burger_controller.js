20b7e1adf9e56c24443359dfb01147202c51e4c0
Orlando Correa <orlandocorrea12@yahoo.com>
2019-01-08 13:40:45 -0500

    finished

---
+++ controllers/burger_controller.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var burger = require('../models/burger.js');
+
+router.get('/', function (req, res) {
+  burger.selectAll(function (data) {
+    var hbsObject = {
+      burgers: data
+    };
+    res.render('index', hbsObject);
+  });
+});
+router.post('/burgers', function (req, res) {
+  burger.insertOne([
+    'burger_name'
+  ], [
+      req.body.burger_name
+    ], function () {
+
+      res.redirect('/');
+    });
+});
+router.post('/burgers/:id', function (req, res) {
+  var condition = 'Id = ' + req.params.id;
+  console.log("condition", condition);
+  burger.updateOne({
+    devoured: true
+  }
+    , condition, function () {
+      console.log(req.params.Id)
+      res.redirect('/');
+    });
+});
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+use burgers_db;
+
+insert into burgers(burger_name, devoured)
+value ('Cheese Burger',false), ('Hamburger', false), ('Double Decker', false);
