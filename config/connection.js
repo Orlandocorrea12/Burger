da19d98ce06edb2ba16da49a07d8fc2b874a0127
Orlando Correa <orlandocorrea12@yahoo.com>
2019-01-08 20:57:33 -0500

    fixing heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,28 @@
 // Require mysql
 var mysql = require("mysql");
- 
+var connection;
 // Set up our connection information
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "jj820qt5lpu6krut.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "bvoqopn77jxv2dxy",
+    password: "fmf2h70ej64ze1yc",
+    database: "	r3b9cpij60d7f2gz"
+  })
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+}
+
 
 // Connect to the database
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
