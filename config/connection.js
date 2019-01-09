57620eccca6315ac20cc1c21f1d9ff968fc18bcb
Orlando Correa <orlandocorrea12@yahoo.com>
2019-01-08 21:28:12 -0500

    finished

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,13 @@ var mysql = require("mysql");
 var connection;
 // Set up our connection information
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection({
-    port: 3306,
-    host: "jj820qt5lpu6krut.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "bvoqopn77jxv2dxy",
-    password: "fmf2h70ej64ze1yc",
-    database: "	r3b9cpij60d7f2gz"
-  })
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
   connection = mysql.createConnection({
     port: 3306,
     host: "localhost",
     user: "root",
-    password: "password",
+    password: process.env.PASSWORD,
     database: "burgers_db"
   });
 }
